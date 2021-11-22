2cd997b8cd83dd0aa6047e40468e954fabdc45c5
Hajïn_ <44238262+AHajin@users.noreply.github.com>
2021-11-22 21:21:38 +0100

    a

--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once "header.php";
-require_once "main.php";
-require_once "footer.php";
-
--- readme.md
+++
@@ -1,3 +0,0 @@
-Site de Pickaria développer par AHajïn avec l'aide de Quozul
-
-- BDD intégré au fichier
